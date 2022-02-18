9563b2991528f2668c24d1afe48752e35e07647b
thumlabero1 <66507256+thumlabero1@users.noreply.github.com>
2022-02-18 16:05:29 +0700

    chunk

--- 02-async/01-set-timeout.js
+++
@@ -1,6 +0,0 @@
-let count = 0
-setInterval (() => console.log ( ` $ { ++ count } mississippi` ), 1000 )
-setTimeout (() => {
-console.log ( 'xin chào từ quá khứ!' )
-process.exit ()
-}, 5500 )
---
+++ 02-async/17-ee-create-emitter.js
@@ -0,0 +1,25 @@
+function createEventSource (url) {
+
+    const source = new EventEmitter()
+    
+    http.get(url, res => {
+    
+    res.on('data', data => {
+    
+    const message = data
+    
+    .toString()
+    
+    .replace(/^data: /, '')
+    
+    .replace(/\n\n$/, '')
+    
+    source.emit('message', message)
+    
+    })
+    
+    })
+    
+    return source
+    
+    }
