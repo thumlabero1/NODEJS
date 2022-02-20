528b426cf81b9bbd1a02bca6e18d48a7371bde0d
thumlabero1 <66507256+thumlabero1@users.noreply.github.com>
2022-02-21 00:30:05 +0700

    error split

---
+++ 02-async/24-transform-csv.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+
+const csv = require('csv-parser')
+
+const { Transform } = require('stream')
+
+const YEAR_MS = 365 * 24 * 60 * 60 * 1000
+
+fs.createReadStream('people.csv')
+
+.pipe(csv())
+
+.pipe(clean())
+
+.on('data', row => console.log(JSON.stringify(row)))
+
+function clean () {
+
+return new Transform({
+
+objectMode: true,
+
+transform (row, encoding, callback) {
+
+const [firstName, lastName] = row.name.split(' ')
+
+const age = Math.floor((Date.now() - new Date(row.dob))/YEAR_MS)
+
+callback(null, {
+
+firstName,
+
+lastName,
+
+age
+
+})
+
+}
+
+})
+
+}
