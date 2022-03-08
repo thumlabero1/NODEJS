621acfee17ea81ea5d8bbd38d08a8e6bd5897f05
thumlabero1 <66507256+thumlabero1@users.noreply.github.com>
2022-03-09 00:20:04 +0700

    mai

--- 04-persistence/02/server.js
+++ 04-persistence/02/server.js
@@ -9,7 +9,6 @@ const middleware = require('./middleware')
 const port = process.env.PORT || 1337
 
 const app = express()
-
 app.use(middleware.cors)
 
 app.use(bodyParser.json())
@@ -17,3 +16,4 @@ app.use(bodyParser.json())
 app.get('/products', api.listProducts)
 
 app.post('/products', api.createProduct)
+const server = app.listen(port, () => console.log(`Server listening on port ${port}`))
