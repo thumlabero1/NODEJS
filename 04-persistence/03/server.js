cd32703a50a645e1b0c781eb549581990ca96ee7
thumlabero1 <66507256+thumlabero1@users.noreply.github.com>
2022-03-09 09:24:38 +0700

    2to3

---
+++ 04-persistence/03/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+
+const api = require('./api')
+
+const middleware = require('./middleware')
+
+const orders = require('./orders')
+
+const port = process.env.PORT || 1337
+
+const app = express()
+app.use(middleware.cors)
+
+app.use(bodyParser.json())
+
+app.get('/products', api.listProducts)
+
+app.post('/products', api.createProduct)
+
+app.get('/orders', api.listOrders)
+
+app.post('/orders', api.createOrder)
+
+const server = app.listen(port, () => console.log(`Server listening on port ${port}`))
