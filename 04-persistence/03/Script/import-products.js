4ad17bafead63401ab0c840c54851b30ca379ecb
thumlabero1 <66507256+thumlabero1@users.noreply.github.com>
2022-03-11 23:56:44 +0700

    add script

---
+++ 04-persistence/03/Script/import-products.js
@@ -0,0 +1,16 @@
+const db = require('../db')
+
+const Products = require('../models/products')
+
+const products = require('../../products.json')
+
+;
+(async function() {
+
+    for (var i = 0; i < products.length; i++) {
+        console.log(await Products.create(products[i]))
+    }
+
+    db.disconnect()
+
+})()
