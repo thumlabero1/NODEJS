8fc2e7bd43574b15f8bf0a9052e317445ad53f86
thumlabero1 <66507256+thumlabero1@users.noreply.github.com>
2022-03-13 16:18:20 +0700

    del

---
+++ 05-authentication/01/Script/import-products.js
@@ -0,0 +1,16 @@
+const db = require('../../../04-persistence/03/db')
+
+const Products = require('../models/products')
+
+const products = require('../../products.json')
+
+;
+(async function() {
+
+    for (var i = 0; i < products.length; i++) {
+        console.log(await Products.create(products[i]))
+    }
+
+    db.disconnect()
+
+})()
