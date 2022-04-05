5000f0bd1a97ba4e06d77cf7db0899146e825c3a
thumlabero1 <66507256+thumlabero1@users.noreply.github.com>
2022-04-05 18:51:25 +0700

    miaka

---
+++ vueapp/movie_rating_app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+  import './assets/stylesheets/main.css';
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+ font-family: 'Avenir', Helvetica, Arial, sans-serif;
+ -webkit-font-smoothing: antialiased;
+ -moz-osx-font-smoothing: grayscale;
+ text-align: center;
+ color: #2c3e50;
+ margin-top: 60px;
+ width: 100%;
+}
+</style>
